4186810fd6b5847eaaa3475db22f66190b8f4602
Warrick Ranis <ranwar26@students.holmen.k12.wi.us>
2025-04-21 21:03:02 -0500

    Drive PID turn fix

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -37,8 +37,8 @@ import java.util.function.DoubleSupplier;
 
 public class DriveCommands {
   private static final double DEADBAND = 0.1;
-  private static final double ANGLE_KP = 0.1;
-  private static final double ANGLE_KD = 0.01;
+  private static final double ANGLE_KP = 0.01;
+  private static final double ANGLE_KD = 0.0;
   private static final double ANGLE_MAX_VELOCITY = 2 * Math.PI;
   private static final double ANGLE_MAX_ACCELERATION = 2 * Math.PI;
   private static final double FF_START_DELAY = 2.0; // Secs
