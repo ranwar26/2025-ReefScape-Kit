6d8b864e583e4b14f30eefe502a1e7e7b02a04cc
Warrick Ranis <ranwar26@students.holmen.k12.wi.us>
2025-05-19 22:35:19 -0500

    Tuned elevator PID to be more in line with a physical robot.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -215,8 +215,8 @@ public class VisionConstants {
     public static final double kRealP = 2.7;
     public static final double kRealI = 0.0;
     public static final double kRealD = 0.0;
-    public static final double kSimP = 2.2;
-    public static final double kSimI = 1.4;
+    public static final double kSimP = 0.8;
+    public static final double kSimI = 1.2;
     public static final double kSimD = 0.2;
 
     // Target length in meters
