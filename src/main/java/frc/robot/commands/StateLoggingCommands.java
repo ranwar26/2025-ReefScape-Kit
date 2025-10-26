5b346168d332ab42930ee923e89cb9267f5b13d8
ranwar26 <123522122+ranwar26@users.noreply.github.com>
2025-10-26 12:39:21 -0500

    Moved pivot into its own Ligament2d for better arm display

--- src/main/java/frc/robot/commands/StateLoggingCommands.java
+++ src/main/java/frc/robot/commands/StateLoggingCommands.java
@@ -29,35 +29,41 @@ public class StateLoggingCommands {
 
     // Part for the current arm
     private static LoggedMechanism2d mechanismCurrent = new LoggedMechanism2d(5, 5.0);
-    private static LoggedMechanismLigament2d elevatorPartCurrent = new LoggedMechanismLigament2d("elevator", 0.0, 0);
+    private static LoggedMechanismLigament2d pivotPartCurrent = new LoggedMechanismLigament2d("pivot", 0.059, 0);
+    private static LoggedMechanismLigament2d elevatorPartCurrent = new LoggedMechanismLigament2d("elevator", 0.0, -90);
     private static LoggedMechanismLigament2d gripperPartCurrent = new LoggedMechanismLigament2d("gripper", 0.388, 0);
     private static LoggedMechanismLigament2d intakePartCurrent = new LoggedMechanismLigament2d("intake", 0.05, 0);
 
     // Part for the target arm
     private static LoggedMechanism2d mechanismTarget = new LoggedMechanism2d(5, 5.0);
-    private static LoggedMechanismLigament2d elevatorPartTarget = new LoggedMechanismLigament2d("elevator", 0.0, 0);
+    private static LoggedMechanismLigament2d pivotPartTarget = new LoggedMechanismLigament2d("pivot", 0.059, 0);
+    private static LoggedMechanismLigament2d elevatorPartTarget = new LoggedMechanismLigament2d("elevator", 0.0, -90);
     private static LoggedMechanismLigament2d gripperPartTarget = new LoggedMechanismLigament2d("gripper", 0.388, 0);
     private static LoggedMechanismLigament2d intakePartTarget = new LoggedMechanismLigament2d("intake", 0.05, 0);
 
     static {
-        mechanismCurrent.getRoot("root", 2.5 - 0.3, 0.2).append(elevatorPartCurrent).append(gripperPartCurrent)
+        mechanismCurrent.getRoot("root", 2.5 - 0.193, 0.337).append(pivotPartCurrent).append(elevatorPartCurrent).append(gripperPartCurrent)
                 .append(intakePartCurrent);
 
+        pivotPartCurrent.setColor(new Color8Bit(0, 255, 0));
         elevatorPartCurrent.setColor(new Color8Bit(0, 255, 0));
         gripperPartCurrent.setColor(new Color8Bit(0, 255, 0));
         intakePartCurrent.setColor(new Color8Bit(0, 255, 0));
 
+        pivotPartCurrent.setLineWeight(4.0);
         elevatorPartCurrent.setLineWeight(4.0);
         gripperPartCurrent.setLineWeight(4.0);
         intakePartCurrent.setLineWeight(4.0);
 
-        mechanismTarget.getRoot("root", 2.5 - 0.3, 0.2).append(elevatorPartTarget).append(gripperPartTarget)
+        mechanismTarget.getRoot("root", 2.5 - 0.193, 0.337).append(pivotPartTarget).append(elevatorPartTarget).append(gripperPartTarget)
                 .append(intakePartTarget);
 
+        pivotPartTarget.setColor(new Color8Bit(255, 0, 0));
         elevatorPartTarget.setColor(new Color8Bit(255, 0, 0));
         gripperPartTarget.setColor(new Color8Bit(255, 0, 0));
         intakePartTarget.setColor(new Color8Bit(255, 0, 0));
 
+        elevatorPartTarget.setLineWeight(3.9);
         elevatorPartTarget.setLineWeight(3.9);
         gripperPartTarget.setLineWeight(3.9);
         intakePartTarget.setLineWeight(3.9);
@@ -76,7 +82,8 @@ public class StateLoggingCommands {
 
         return Commands.run(
                 () -> {
-                    elevatorPartCurrent.setAngle(Math.toDegrees(pivot.getCurrentAngle()));
+                    pivotPartCurrent.setAngle(Math.toDegrees(pivot.getCurrentAngle() + 90.0));
+
                     elevatorPartCurrent.setLength(elevator.getCurrentLength());
 
                     gripperPartCurrent.setAngle(Math.toDegrees(-wrist.getCurrentAngle()) + 90.0);
@@ -101,7 +108,8 @@ public class StateLoggingCommands {
 
         return Commands.run(
                 () -> {
-                    elevatorPartTarget.setAngle(Math.toDegrees(pivot.getTargetAngle()));
+                    pivotPartTarget.setAngle(Math.toDegrees(pivot.getTargetAngle()));
+
                     elevatorPartTarget.setLength(elevator.getTargetLength());
 
                     gripperPartTarget.setAngle(Math.toDegrees(-wrist.getTargetAngle()) + 90.0);
