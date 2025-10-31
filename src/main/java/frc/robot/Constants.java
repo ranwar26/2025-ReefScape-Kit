b3be8f1557574d92e09da7c138fda93911778315
ranwar26 <123522122+ranwar26@users.noreply.github.com>
2025-10-31 14:53:00 -0500

    Set kG values for pivot and wrist

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,8 +28,10 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotBase;
 
 /**
- * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
- * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics sim) and "replay"
+ * This class defines the runtime mode used by AdvantageKit. The mode is always
+ * "real" when running
+ * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics
+ * sim) and "replay"
  * (log replay from a file).
  */
 public final class Constants {
@@ -54,13 +56,12 @@ public final class Constants {
     public static final double trackWidth = Units.inchesToMeters(29.0);
     public static final double wheelBase = Units.inchesToMeters(29.0);
     public static final double driveBaseRadius = Math.hypot(trackWidth / 2.0, wheelBase / 2.0);
-    public static final Translation2d[] moduleTranslations =
-        new Translation2d[] {
-          new Translation2d(trackWidth / 2.0, wheelBase / 2.0),
-          new Translation2d(trackWidth / 2.0, -wheelBase / 2.0),
-          new Translation2d(-trackWidth / 2.0, wheelBase / 2.0),
-          new Translation2d(-trackWidth / 2.0, -wheelBase / 2.0)
-        };
+    public static final Translation2d[] moduleTranslations = new Translation2d[] {
+        new Translation2d(trackWidth / 2.0, wheelBase / 2.0),
+        new Translation2d(trackWidth / 2.0, -wheelBase / 2.0),
+        new Translation2d(-trackWidth / 2.0, wheelBase / 2.0),
+        new Translation2d(-trackWidth / 2.0, -wheelBase / 2.0)
+    };
 
     // Zeroed rotation values for each module, see setup instructions.
     public static final Rotation2d frontLeftZeroRotation = new Rotation2d(-Math.PI / 2.0);
@@ -82,15 +83,15 @@ public final class Constants {
     // Drive motor configuration
     public static final int driveMotorCurrentLimit = 40;
     public static final double wheelRadiusMeters = Units.inchesToMeters(1.5);
-    public static final double driveMotorReduction =
-        (45.0 * 22.0) / (13.0 * 15.0); // MAXSwerve with 13 pinion teeth and 22 spur teeth
+    public static final double driveMotorReduction = (45.0 * 22.0) / (13.0 * 15.0); // MAXSwerve with 13 pinion teeth
+                                                                                    // and 22 spur teeth
     public static final DCMotor driveGearbox = DCMotor.getNEO(1);
 
     // Drive encoder configuration
-    public static final double driveEncoderPositionFactor =
-        2 * Math.PI / driveMotorReduction; // Rotor Rotations -> Wheel Radians
-    public static final double driveEncoderVelocityFactor =
-        (2 * Math.PI) / 60.0 / driveMotorReduction; // Rotor RPM -> Wheel Rad/Sec
+    public static final double driveEncoderPositionFactor = 2 * Math.PI / driveMotorReduction; // Rotor Rotations ->
+                                                                                               // Wheel Radians
+    public static final double driveEncoderVelocityFactor = (2 * Math.PI) / 60.0 / driveMotorReduction; // Rotor RPM ->
+                                                                                                        // Wheel Rad/Sec
 
     // Drive PID configuration
     public static final double driveKp = 0.0;
@@ -125,58 +126,54 @@ public final class Constants {
     public static final double robotMassKg = 50.219;
     public static final double robotMOI = 4.859;
     public static final double wheelCOF = 1.190;
-    public static final RobotConfig ppConfig =
-        new RobotConfig(
-            robotMassKg,
-            robotMOI,
-            new ModuleConfig(
-                wheelRadiusMeters,
-                maxSpeedMetersPerSec - 0.8,
-                wheelCOF,
-                driveGearbox.withReduction(driveMotorReduction),
-                driveMotorCurrentLimit,
-                1),
-            moduleTranslations);
-}
+    public static final RobotConfig ppConfig = new RobotConfig(
+        robotMassKg,
+        robotMOI,
+        new ModuleConfig(
+            wheelRadiusMeters,
+            maxSpeedMetersPerSec - 0.8,
+            wheelCOF,
+            driveGearbox.withReduction(driveMotorReduction),
+            driveMotorCurrentLimit,
+            1),
+        moduleTranslations);
+  }
 
-public class VisionConstants {
-  // AprilTag layout
-  public static AprilTagFieldLayout aprilTagLayout =
-      AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+  public class VisionConstants {
+    // AprilTag layout
+    public static AprilTagFieldLayout aprilTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
-  // Camera names, must match names configured on coprocessor
-  public static String camera0Name = "camera_0";
-  public static String camera1Name = "camera_1";
+    // Camera names, must match names configured on coprocessor
+    public static String camera0Name = "camera_0";
+    public static String camera1Name = "camera_1";
 
-  // Robot to camera transforms
-  // (Not used by Limelight, configure in web UI instead)
-  public static Transform3d robotToCamera0 =
-      new Transform3d(-0.5, 0.20, 0.0, new Rotation3d(0.0, Math.toRadians(-15.0), 0.0));
-  public static Transform3d robotToCamera1 =
-      new Transform3d(-0.5, -0.20, 0.0, new Rotation3d(0.0, Math.toRadians(-15.0), 0.0));
+    // Robot to camera transforms
+    // (Not used by Limelight, configure in web UI instead)
+    public static Transform3d robotToCamera0 = new Transform3d(-0.5, 0.20, 0.0,
+        new Rotation3d(0.0, Math.toRadians(-15.0), 0.0));
+    public static Transform3d robotToCamera1 = new Transform3d(-0.5, -0.20, 0.0,
+        new Rotation3d(0.0, Math.toRadians(-15.0), 0.0));
 
-  // Basic filtering thresholds
-  public static double maxAmbiguity = 0.3;
-  public static double maxZError = 0.75;
+    // Basic filtering thresholds
+    public static double maxAmbiguity = 0.3;
+    public static double maxZError = 0.75;
 
-  // Standard deviation baselines, for 1 meter distance and 1 tag
-  // (Adjusted automatically based on distance and # of tags)
-  public static double linearStdDevBaseline = 0.02; // Meters
-  public static double angularStdDevBaseline = 0.06; // Radians
+    // Standard deviation baselines, for 1 meter distance and 1 tag
+    // (Adjusted automatically based on distance and # of tags)
+    public static double linearStdDevBaseline = 0.02; // Meters
+    public static double angularStdDevBaseline = 0.06; // Radians
 
-  // Standard deviation multipliers for each camera
-  // (Adjust to trust some cameras more than others)
-  public static double[] cameraStdDevFactors =
-      new double[] {
+    // Standard deviation multipliers for each camera
+    // (Adjust to trust some cameras more than others)
+    public static double[] cameraStdDevFactors = new double[] {
         1.0, // Camera 0
         1.0 // Camera 1
-      };
+    };
 
-  // Multipliers to apply for MegaTag 2 observations
-  public static double linearStdDevMegatag2Factor = 0.5; // More stable than full 3D solve
-  public static double angularStdDevMegatag2Factor =
-      Double.POSITIVE_INFINITY; // No rotation data available
-}
+    // Multipliers to apply for MegaTag 2 observations
+    public static double linearStdDevMegatag2Factor = 0.5; // More stable than full 3D solve
+    public static double angularStdDevMegatag2Factor = Double.POSITIVE_INFINITY; // No rotation data available
+  }
 
   public final class PivotConstants {
 
@@ -199,7 +196,7 @@ public class VisionConstants {
 
     // These are the voltages of the motors to overcome gravity to reduce sliding
     public static final double kRealG = 0.14;
-    public static final double kSimG = 0.0; // TODO: test sim value
+    public static final double kSimG = 0.32;
 
     // Target angle in degrees converted to radians
     public static final double kHomeAngle = Math.toRadians(40.0);
@@ -279,7 +276,7 @@ public class VisionConstants {
 
     // These are the voltages of the motors to overcome gravity to reduce sliding
     public static final double kRealG = 0.13;
-    public static final double kSimG = 0.0; //TODO: Test this
+    public static final double kSimG = 0.0;
 
     // Target angle in degrees converted to radians
     public static final double kHomeAngle = Math.toRadians(0.0);
